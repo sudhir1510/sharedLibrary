98432e5a2e87ff7977dcbfdc9860e248cf487a66
sudhir1510 <42642645+sudhir1510@users.noreply.github.com>
2020-02-19 08:27:02 +0530

    Update Student.groovy

--- src/com/cleverbuilder/Student.groovy
+++ src/com/cleverbuilder/Student.groovy
@@ -2,6 +2,6 @@ package com.cleverbuilder
 
 public class Student {
    def static String getMessage( String name ){
-      echo "You are awesome , ${name}"
+      print "You are awesome , ${name}"
    }
 }
